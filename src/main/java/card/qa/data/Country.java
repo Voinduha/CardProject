0feb567b8e8c9e2e53152e7a440e491504aec72d
DanVu <danvu@ya.ru>
2021-07-29 18:56:24 +0300

    added constructor in enum class

--- src/main/java/card/qa/data/Country.java
+++ src/main/java/card/qa/data/Country.java
@@ -1,5 +1,14 @@
 package card.qa.data;
 
 public enum Country {
-    Russia, China, USA
+    Russia("Россия"), China("Китай"), USA("США");
+    private String countryName;
+
+    Country(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
 }
